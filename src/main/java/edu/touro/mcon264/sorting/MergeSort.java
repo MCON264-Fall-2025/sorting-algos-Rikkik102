fc0f8cc5020632f3f5a8af0e75744d16134cb781
Rivka Kaufman <rkaufman8@student.touro.edu>
2025-12-29 19:22:23 -0500

    implemented sorting methods

--- src/main/java/edu/touro/mcon264/sorting/MergeSort.java
+++ src/main/java/edu/touro/mcon264/sorting/MergeSort.java
@@ -6,6 +6,34 @@ public class MergeSort implements Sorter {
 
     @Override
     public <T> void sort(T[] a, Comparator<? super T> comp) {
-        // TODO: implement merge sort
-    }
+        if (a == null || a.length <= 1) return;
+
+        @SuppressWarnings("unchecked")
+        T[] aux= (T[]) new Object[a.length];
+        class MergeSorter {
+            void mergeSort(int left, int right) {
+                if (left >= right) return;
+                int mid = left + (right - left) / 2;
+                mergeSort(left, mid);
+                mergeSort(mid + 1, right);
+                for (int i = left; i <= right; i++) {
+                    aux[i] = a[i];
+                }
+                int i = left;
+                int j = mid + 1;
+                int k = left;
+                while (i <= mid && j <= right) {
+                    if (comp.compare(aux[i], aux[j]) <= 0) {
+                        a[k++] = aux[i++];
+                    } else {
+                        a[k++] = aux[j++];
+                    }
+                }
+                while (i <= mid) {
+                    a[k++] = aux[i++];
+                }
+            }
+        }
+        new MergeSorter().mergeSort(0, a.length - 1);
+            }
 }
